7d0397da47cb884e61d3e0a12940369710979250
drjoeCV <30839834+drjoeCV@users.noreply.github.com>
2017-11-09 10:04:25 -0800

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+def isPalindrome(s):
+    # Create the reverse string
+    reverse = s[::-1]
+ 
+    # Checking if both string are equal or not
+    if (s == rev):
+        return True
+    return False
+ 
+ 
+# Driver code
+s = "malayalam"
+ans = isPalindrome(s)
+ 
+if ans == 1:
+    print("Yes")
+else:
+    print("No")
